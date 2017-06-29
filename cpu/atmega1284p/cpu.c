85205725a9e974b3699ca94e5698da7645af4eba
Robert Hartung <hartung@ibr.cs.tu-bs.de>
2017-06-29 09:32:07 +0200

    Adds i2c support to the inga boards and adds adxl configuration

--- cpu/atmega1284p/cpu.c
+++ cpu/atmega1284p/cpu.c
@@ -48,35 +48,4 @@ void cpu_init(void)
   wdt_disable();
 
   periph_init();
-
-  //
-  // /// clock init
-  // cli();
-  // /* Clock from crystal on TOSC0-1 */
-  // ASSR = _BV(AS2);
-  // /* Set counter to zero */
-  // TCNT0 = 0;
-  // /*
-  //  * Set comparison register:
-  //  * Crystal freq. is 32768,
-  //  * pre-scale factor is 8, we want CLOCK_CONF_SECOND ticks / sec:
-  //  * 32768 = 8 * CLOCK_CONF_SECOND * OCR2A, less 1 for CTC mode
-  //  */
-  // OCR2A = 8000000UL/1024UL/1024;
-  // /*
-  //  * Set timer control register:
-  //  *  - prescale: 8 (CS21)
-  //  *  - counter reset via comparison register (WGM21)
-  //  */
-  // TCCR2A = _BV(WGM21);
-  // TCCR2B = _BV(CS21);
-  // /* Clear interrupt flag register */
-  // TIFR2 = TIFR2;
-  // /*
-  //  * Raise interrupt when value in OCR2 is reached. Note that the
-  //  * counter value in TCNT2 is cleared automatically.
-  //  */
-  // TIMSK2 = _BV(OCIE2A);
-  //
-  // sei();
 }
