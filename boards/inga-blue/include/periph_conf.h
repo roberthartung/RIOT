1f05f8eeaea59b8fe82c5136b99a71bdc13e4638
Robert Hartung <hartung@ibr.cs.tu-bs.de>
2017-07-28 00:55:04 +0200

    boards/inga-blue: new board

---
+++ boards/inga-blue/include/periph_conf.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2014-2016 Freie Universität Berlin
+ *               2014 Hinnerk van Bruinehsen
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     boards_arduino-mega2560
+ * @{
+ *
+ * @file
+ * @brief       Peripheral MCU configuration for the Arduino Mega 2560 board
+ *
+ * @author      Hinnerk van Bruinehsen <h.v.bruinehsen@fu-berlin.de>
+ * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
+ */
+
+#ifndef PERIPH_CONF_H_
+#define PERIPH_CONF_H_
+
+#include "periph_conf_common.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define RTT_NUMOF        1
+#define RTT_FREQUENCY    1000
+#define RTT_MAX_VALUE    (0xff) // ?
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PERIPH_CONF_H_ */
