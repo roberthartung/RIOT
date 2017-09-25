5f6044502636914f77b38b68a24898b2126c810a
Robert Hartung <hartung@ibr.cs.tu-bs.de>
2017-09-25 10:19:44 +0200

    boards/inga: adds all four versions

---
+++ boards/inga_blue/include/board.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2016 Robert Hartung <hartung@ibr.cs.tu-bs.de>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+#ifndef INGA_COMMON_BOARD_H_
+#define INGA_COMMON_BOARD_H_
+
+#include "cpu.h"
+#include "periph_conf.h"
+/* For GPIO_IN */
+#include "periph/gpio.h"
+/* from inga_common */
+#include "board_common.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* the blue inga does not have any LEDs or buttons */
+
+#ifdef __cplusplus
+}
+#endif
+
+/** @} */
+#endif /*  INGA_COMMON_BOARD_H_ */
